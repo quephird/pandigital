ae7115189729d19e4942e3ad7628ebe400c6c645
quephird <danielle_kefford@hotmail.com>
2016-01-16 19:18:43 -0500

    Purdified the output.

--- src/Main.hs
+++ src/Main.hs
@@ -7,15 +7,20 @@ pairs [] = []
 pairs [x] = []
 pairs (x1:x2:xs) = (x1,x2) : pairs xs
 
-exponentialSum :: Floating a => [(a,a)] -> a
-exponentialSum = foldl (+) 0 . map (\(b,e) -> b ** e)
+data ExponentialSum = ExponentialSum [(Int, Int)]
 
-showExponentialSum :: (RealFrac a, Show a) => [(a,a)] -> String
-showExponentialSum = intercalate " + " . map (\(b,e) -> (show $ round b) ++ "^" ++ (show $ round e))
+instance Show ExponentialSum where
+  show (ExponentialSum ds) = intercalate " + " $ map (\(b,e) -> (show b) ++ (lookup e)) ds where
+    lookup e = ["⁰¹²³⁴⁵⁶⁷⁸⁹" !! e]
 
-findAllPandigitalSums :: (Floating a, Ord a) => a -> [[(a, a)]]
-findAllPandigitalSums n = nub $ filter (\ds -> exponentialSum ds == n) $ map (sort . pairs) $ permutations [0,1,2,3,4,5,6,7,8,9]
+exponentialSum :: [(Int, Int)] -> Int
+exponentialSum = foldl (+) 0 . map (\(b,e) -> b ^ e)
+
+findAllPandigitalSums :: Int -> [ExponentialSum]
+findAllPandigitalSums n = map ExponentialSum $ nub $ filter (\ds -> exponentialSum ds == n) $ map (sort . pairs) $ permutations [0,1,2,3,4,5,6,7,8,9]
 
 main = do
-  mapM (putStrLn . showExponentialSum) $ findAllPandigitalSums 2016
-
+  putStrLn "Enter a year:"
+  input <- getLine
+  let year = read input :: Int in
+    mapM (putStrLn . show) $ findAllPandigitalSums year
